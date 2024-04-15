2332b3f47f51767583838609b3681a796b859d00
Magic <manaressam220@gmail.com>
2024-04-15 17:22:39 +0200

    islamiApp_quraan fragment

--- app/src/main/java/com/manar/islamiapp/fragment/TasbehFragment.kt
+++ app/src/main/java/com/manar/islamiapp/fragment/TasbehFragment.kt
@@ -5,11 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.manar.islamiapp.R
 import com.manar.islamiapp.databinding.FragmentQuraanBinding
 import com.manar.islamiapp.databinding.FragmentTasbehBinding
 
 class TasbehFragment:Fragment() {
     lateinit var binding: FragmentTasbehBinding
+    var counter = 0
+    var currentDhikrIndex =0
+
+    lateinit var azkarList : MutableList<String>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +23,28 @@ class TasbehFragment:Fragment() {
     ): View?{
         binding =FragmentTasbehBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        azkarList = resources.getStringArray(R.array.azkarList).toMutableList()
+        onSebhaClick()
+    }
+
+    private fun onSebhaClick() {
+        binding.bodySebha.setOnClickListener{
+            binding.bodySebha.rotation+= (360/33).toFloat()
+            if(counter<33){
+                counter++
+            }
+            else {
+                counter=0
+                currentDhikrIndex = if (currentDhikrIndex<azkarList.size-1)++currentDhikrIndex else 0
+                binding.dhikr.text= azkarList[currentDhikrIndex]
+
+            }
+            binding.counterDhikr.text=counter.toString()
+        }
     }
 
 }
